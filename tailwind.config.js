9a5727f9b7b772c617227988286065433b0cfa8e
nkknz <nikka.lanaza15@gmail.com>
2024-07-03 18:06:41 +0800

    adding repo for heroku

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./templates/*.{html,htm}, './static/css/*.css', './static/src/*.css'"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
