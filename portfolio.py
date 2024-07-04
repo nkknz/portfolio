c8f877d8a76f68cf19c67a7650657849f5c60d34
nkknz <nikka.lanaza15@gmail.com>
2024-07-04 19:14:31 +0800

    added skillsets

--- portfolio.py
+++ portfolio.py
@@ -15,6 +15,10 @@ def profile():
 def projects():
     return render_template('projects.html')
 
+@app.route("/skillsets")
+def skillsets():
+    return render_template('skillsets.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
